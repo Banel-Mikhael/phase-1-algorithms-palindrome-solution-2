853bddcc4b2e17f0281630208d7277a8bf3b4ec9
banelmikhael <banel.mikhael.bm@gmail.com>
2022-11-23 14:53:50 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // convert string to an array and splits the word for seperate letters
+  const arrayValues = word.split("");
+  // reverse the array values or the letters
+  const reverseArrayValues = arrayValues.reverse();
+  // convert the array to string nad put them together
+  const reverseString = reverseArrayValues.join("");
+
+  //  make a condition
+  //  to check if the wanted word is the same is the reverced word we made if true its palindrome if not it not palindrom
+  if (word == reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+   1-take whole word
+  2-split whole word to pieces so you can use word by word
+  3-reverse the chars u splited 
+  4-put the reversed chars together 
+  5-make a condition to see if main word is equal too reversed word if true its palindrome if not its not palindrome
 */
 
 /*
   Add written explanation of your solution here
+  we have to split the word given by the user and split it into several letters to let us iterate over the whole word and try
+  to make a first condition to check if the first letter is equal to last letter and with the reverse built in function we reverse the whole
+  word so that how we compare between main word and reversed word if both are same it will be a palindrome word
 */
 
 // You can run `node index.js` to view these console logs
--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@ global.expect = chai.expect;
 const isPalindrome = require("../index");
 
 describe("isPalindrome", () => {
-  it("returns true for 'abba'", () => {
-    expect(isPalindrome("abba")).to.be.true;
+  it("returns true for 'bob'", () => {
+    expect(isPalindrome("bob")).to.be.true;
   });
   it("returns true for 'racecar'", () => {
     expect(isPalindrome("racecar")).to.be.true;
@@ -12,8 +12,14 @@ describe("isPalindrome", () => {
   it("returns true for 'a'", () => {
     expect(isPalindrome("a")).to.be.true;
   });
+  it("returns true for 'level'", () => {
+    expect(isPalindrome("level")).to.be.true;
+  });
   it("returns false for 'robot'", () => {
     expect(isPalindrome("robot")).to.be.false;
+  });
+  it("returns false for 'shalom'", () => {
+    expect(isPalindrome("shalom")).to.be.false;
   });
   it("returns false for 'ab'", () => {
     expect(isPalindrome("ab")).to.be.false;
